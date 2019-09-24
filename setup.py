473e5ebf4ca8e22f7039b67d5c6b9219a338f611
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2019-09-24 18:25:05 +0200

    Updated requirements.txt and setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,25 @@
-from setuptools import setup
+import setuptools
 
-with open('requirements.txt') as f:
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+with open('requirements.txt', 'r') as f:
     requirements = f.read().splitlines()
 
-setup(
+setuptools.setup(
     name="gym_tak",
-    version='0.0.1',
+    version='0.0.2',
+    author='DrSmugleaf',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/DrSmugleaf/gym-tak',
+    packages=setuptools.find_packages(),
     install_requires=requirements,
-    license='MIT'
+    license='MIT',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6'
 )
